bc615d09761f6abc8a98ffe9b2ce6b69c5ec31d6
Cayden <cayden.simler@hotmail.com>
2017-06-02 17:35:15 -0500

    Added a simple hover animation to navbar.

--- public/js/main.js
+++ public/js/main.js
@@ -2,16 +2,38 @@ $(document).ready(function(){
 
 	"use strict";
 
-	var originalHTML = {};
+	// var originalHTML = {};
 
+
+	// $('.navbar a').hover(function(){
+	// 	originalHTML.html = $(this).html();
+
+	// 	$(this).append(' <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>');
+	// }, function(){
+	// 	$(this).html(originalHTML.html);
+	// });	
 
 	$('.navbar a').hover(function(){
-		originalHTML.html = $(this).html();
-
-		$(this).append(' <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>');
+		$(this).animate({
+			backgroundColor: "#1E3333",
+			color: "white",
+			fontSize: "20px",
+			paddingTop: "-=.4%"
+		}, 500).animate({
+			borderBottomWidth: "1px",
+			borderTopWidth: "1px"
+		}, 50);
 	}, function(){
-		$(this).html(originalHTML.html);
-	});	
+		$(this).animate({
+			borderBottomWidth: "0px",
+			borderTopWidth: "0px"
+		}, 50).animate({
+			color: "#97AAAF",
+			backgroundColor: "#305252",
+			fontSize: "15px",
+			paddingTop: "+=.4%"
+		}, 500);
+	});
 
 
 
